a9ff9c3f07338c60ecd7ef456ff5c753a166ba9e
Leo Silva Souza <leovano@protonmail.com>
2018-12-03 12:46:35 -0200

    fix

--- 2018/03_1/ruby/leovano.rb
+++ 2018/03_1/ruby/leovano.rb
@@ -2,25 +2,26 @@
 
 begin
   input = File.readlines(ARGV[0])
-rescue TypeError
-  STDERR.puts 'Arg1'
-  exit
-end
 
-claims = Hash.new 0
+  claims = Hash.new 0
 
-input.map! do |claim|
-  claim.split(/[#@,:x]/)
-       .reject(&:empty?)
-       .map(&:to_i)
-end
+  input.map! do |claim|
+    claim.split(/[#@,:x]/)
+         .reject(&:empty?)
+         .map(&:to_i)
+  end
 
-input.each do |claim|
-  (claim[1]...claim[1] + claim[3]).each do |x|
-    (claim[2]...claim[2] + claim[4]).each do |y|
-      claims[[x, y]] += 1
+  input.each do |claim|
+    (claim[1]...claim[1] + claim[3]).each do |x|
+      (claim[2]...claim[2] + claim[4]).each do |y|
+        claims[[x, y]] += 1
+      end
     end
   end
-end
 
-puts claims.values.count { |v| v > 1 }
+  puts(claims.values.count { |v| v > 1 })
+rescue TypeError
+  STDERR.puts 'Arg1'
+rescue StandardError => e
+  STDERR.puts e
+end
